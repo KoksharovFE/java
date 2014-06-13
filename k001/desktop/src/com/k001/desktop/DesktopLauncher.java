6760b65bca86913d8e2b9b757b9b38cce309396b
KoksharovFE <konnor2007@gmail.com>
2014-06-13 18:30:50 +0400

    +libjdx test project

---
+++ k001/desktop/src/com/k001/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.k001.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.k001.k001;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new k001(), config);
+	}
+}
